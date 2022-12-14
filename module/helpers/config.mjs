f79e5ef5e1ef0e2b083d6d8519a54c836f7474f2
Jeremiah Jordan <jeremiah@ravn.com>
2022-12-14 01:08:35 -0800

    initial commit of earth land cortex system

---
+++ module/helpers/config.mjs
@@ -0,0 +1,27 @@
+export const earthland = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ earthland.attributes = {
+  "strength": "earthland.AbilityStr",
+  "precision": "earthland.AbilityPre",
+  "speed": "earthland.AbilitySpd",
+  "constitution": "earthland.AbilityCon",
+  "intelligence": "earthland.AbilityInt",
+  "perception": "earthland.AbilityPer",
+  "willpower": "earthland.AbilityWil",
+  "charisma": "earthland.AbilityCha"
+};
+
+earthland.abilityAbbreviations = {
+  "strength": "earthland.AbilityStrAbbr",
+  "precision": "earthland.AbilityPreAbbr",
+  "speed": "earthland.AbilitySpdAbbr",
+  "con": "earthland.AbilityConAbbr",
+  "int": "earthland.AbilityIntAbbr",
+  "perception": "earthland.AbilityPerAbbr",
+  "willpower": "earthland.AbilityWilAbbr",
+  "cha": "earthland.AbilityChaAbbr"
+};
