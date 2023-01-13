d859d5f4455a2a8ebba90865fcf935e49f998d14
Jeremiah Jordan <jeremiah@ravn.com>
2023-01-12 17:00:56 -0800

    added back in the other spells

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -191,7 +191,12 @@ export class earthlandActorSheet extends ActorSheet {
       1: [],
       2: [],
       3: [],
-      4: []
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: []
     };
     const dicepools = [];
 
